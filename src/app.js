35493d61b765559cdfaa9e7f71f26b75b20eff52
frozty03 <alvesfrozadavi@gmail.com>
2025-10-21 17:45:53 -0300

    Practiced the methods put and delete

--- src/app.js
+++ src/app.js
@@ -18,6 +18,12 @@ const books = [ // simulate a BD
     }
 ]
 
+function findBook(id) {
+    return books.findIndex(book => { // findIndex tests every book to the arrow function
+        return book.id === Number(id);
+    })
+}
+
 app.get('/', (req, res) => {
     res.status(200).send("Test Server Node.js");
 });
@@ -26,9 +32,28 @@ app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
 
+app.get('/books/:id', (req, res) => { // :id is a variable
+    const index = findBook(req.params.id); // variable defined in get method
+    res.status(200).json(books[index]);
+})
+
 app.post('/books', (req, res) => {
     books.push(req.body); // body (data to create a new object)
     res.status(201).send('book created');
 });
+
+app.put('/books/:id', (req, res) => {
+    const index = findBook(req.params.id);
+    books[index].name = req.body.name;
+    books[index].author = req.body.author;
+    res.status(200).json(books[index]);
+})
+
+app.delete('/books/:id', (req, res) => {
+    const index = findBook(req.params.id);
+    books.splice(index, 1);
+    res.status(200).send('Book deleted')
+})
+
 
 export default app;
